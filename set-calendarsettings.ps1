c69cb51dc7ddf52fc0de3e9c0d522c8c416cf7aa
Webster Massingham <wmassingham@gmail.com>
2019-12-26 09:43:41 -0500

    add section controls, change to upn for identity

--- set-calendarsettings.ps1
+++ set-calendarsettings.ps1
@@ -2,6 +2,8 @@
 # Loops through all mailboxes and sets default permission to publishing editor
 # 2017-10-19 added room, equipment mailboxes
 # 2018-11-28 add 365 connection
+# 2019-07-08 add flags to control each section
+# 2019-12-26 change from name to userprincipalname to handle two objects with the same name
 
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
 
@@ -11,18 +13,22 @@ $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri ht
 Import-PSSession $Session
 
 # Set calendar permissions on mailboxes
-$mailboxes = get-mailbox -recipienttype usermailbox,roommailbox,equipmentmailbox
-ForEach ($Mailbox in $mailboxes) { Set-MailboxFolderPermission -user default -Identity "$($Mailbox.Name):\Calendar" -accessrights publishingeditor }
+if ($setpublishingeditor -eq "true") {
+	$mailboxes = get-mailbox -recipienttype usermailbox,roommailbox,equipmentmailbox
+	ForEach ($Mailbox in $mailboxes) { Set-MailboxFolderPermission -user default -Identity "$($Mailbox.userprincipalname):\Calendar" -accessrights publishingeditor }
+}
 
 # Set calendar processing settings on room & equipment mailboxes
-$rooms = get-mailbox -recipienttype roommailbox,equipmentmailbox
-foreach ($room in $rooms) {
-	set-calendarprocessing -identity "$($room.Name)" `
-		-AllowConflicts $false `
-		-AutomateProcessing AutoAccept `
-		-BookingWindowInDays 365 `
-		-AddOrganizerToSubject $false `
-		-DeleteSubject $false `
-		-DeleteComments $false `
-		-RemovePrivateProperty $false
+if ($setcalendarprocessing -eq "true") {
+	$rooms = get-mailbox -recipienttype roommailbox,equipmentmailbox
+	foreach ($room in $rooms) {
+		set-calendarprocessing -identity "$($room.userprincipalname)" `
+			-AllowConflicts $false `
+			-AutomateProcessing AutoAccept `
+			-BookingWindowInDays 365 `
+			-AddOrganizerToSubject $false `
+			-DeleteSubject $false `
+			-DeleteComments $false `
+			-RemovePrivateProperty $false
+	}
 }
